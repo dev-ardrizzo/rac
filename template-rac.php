30328696808aeba2acfe84e42fa17124e09995ca
Luan Ardrizzo <luan.ardrizzo@gmail.com.br>
2020-07-10 15:50:35 -0300

    Add - Nova coluna [Lib. De Conteudo]

--- template-rac.php
+++ template-rac.php
@@ -77,7 +77,7 @@ get_header(); ?>
 						</div>
 						<div class="texto">
 							<span class="large">100% on-line</span>
-							<p>(app MEDSOFT Pro)</p>
+							<p style="margin-bottom:47px">(app MEDSOFT Pro)</p>
 						</div>
 					</div>
 	
@@ -91,15 +91,15 @@ get_header(); ?>
 						</div>
 					</div>
 	
-					<!-- <div class="box__infosProduto">
+					<div class="box__infosProduto">
 						<div class="icone">
 							<img src="<?php bloginfo('template_directory'); ?>-child/imgs/icone-calendario.svg" alt="" class="icone__calendario">
 						</div>
 						<div class="texto">
-							<span class="large">1 apostila DIGITAL</span>
-							<p><span class="large">por semana</span><br>(3 semanas)</p>
+							<span class="large">Liberação de conteúdo</span>
+							<p style="margin-bottom:47px">Disponível a partir de 02/10</p>
 						</div>
-					</div> -->
+					</div>
 				</div>
 			</div>
 	
